de6cd2b785ef463def7b69bc6cabd75c0c6d5522
Coazan <rc19106@ues.edu.sv>
2024-04-21 17:52:19 -0600

    NavBar working

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,28 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import "./NavBar.css";
-import vital from "../../assets/vital.jpg";
+import vital from "../../assets/vital.jpg"
+
+
 import { NavLink } from "react-router-dom";
 
 export function NavBar() {
-  const [isHidden, setIsHidden] = useState(true); // Initially hidden
-
-  const toggleNavbar = () => {
-    setIsHidden(!isHidden);
-  };
 
   return (
     <>
-      {isHidden ? null : (
-        <nav className={`navbar navbar-expand-sm bg-dark bg-gradient ${isHidden ? 'hidden' : ''}`}>
-          <div className="container-fluid pe-3 ps-3">
-            {/* Logo or brand */}
-            <div className="ico d-flex">
-              <NavLink to="/home" className="nav-link" data-bs-toggle="pill">
-                <img id="logo" src={vital} className="rounded-circle" alt="Logo" />
-              </NavLink>
-            </div>
-            {/* Navigation links */}
-            <ul className="nav nav-pills">
+      <nav className="navbar m-0 navbar-expand-sm bg-dark bg-gradient">
+        <div className="container-fluid pe-3 ps-3">
+          <div className="ico d-flex ">
+          <NavLink to="/home"
+          className={"nav-link"} 
+          data-bs-toggle={"pill"}>
+            <img
+              id="logo"
+              src={vital}
+              className="rounded-circle"
+            />
+            </NavLink>
+          </div>
+          <ul className="nav nav-pills">
               <li className="nav-item">
                 <NavLink to="/home" className="nav-link" data-bs-toggle="pill">
                   <i className="fa-solid fa-house"></i> Inicio
@@ -38,18 +38,9 @@ export function NavBar() {
                   <i className="fa-solid fa-magnifying-glass"></i> Buscar
                 </NavLink>
               </li>
-            </ul>
-            {/* Toggle button */}
-            <button onClick={toggleNavbar} className="navbar-toggler">
-              <span className="sr-only">Toggle navigation</span>
-              <i className="fa-solid fa-bars"></i>
-            </button>
-          </div>
-        </nav>
-      )}
-      <button className="show-navbar-btn" onClick={toggleNavbar}>
-        Mostrar Menú
-      </button>
+            </ul> 
+        </div>
+      </nav>
     </>
   );
 }
