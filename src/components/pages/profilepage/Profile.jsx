841142764c9a3ac8d6a48068c0b396c94aae7813
Coazan <rc19106@ues.edu.sv>
2024-04-21 17:50:58 -0600

    profile upgraded

--- src/components/pages/profilepage/Profile.jsx
+++ src/components/pages/profilepage/Profile.jsx
@@ -9,7 +9,7 @@ export const Profile = () => {
   const [isActive, setIsActive] = useState(false);
   const [id, setId] = useState();
   const [name, setName] = useState("Vital")
-  const [description, setDescription] = useState("The best way to listening to music.")
+  const [description, setDescription] = useState("The best way to listen to music.")
   const [about, setAbout] = useState("I was developed for some students at FUSALMO'S frontend course, but I would really like to be a real app.")
   const [favorites, setfavorites] = useState("rock, country")
 
@@ -18,7 +18,7 @@ export const Profile = () => {
     setId(newId)
   }
 
-  const handleEdit = (Id, value) => {
+  const Edit = (Id, value) => {
     switch (Id) {
       case 1:
         setName(value);
@@ -40,41 +40,41 @@ export const Profile = () => {
   return (
     <>
       <div className="container-fluid ">
-        {console.log(isActive)}
-        <div className="row p-4 d-flex justify-content-center">
-          {isActive ? <TextArea ActivateEdit={ActivateEdit} id={id} handleEdit={handleEdit} /> : ""}
-          <div className="columna col-12 col-lg-8 d-flex justify-content-center" >
-            <article className="card bg-dark pt-3  mt-3">
+
+        <div className="row p-4 d-flex ">
+          <div className="columna1 col-12 col-lg-6 mb-3" >
+            <article className="card bg-dark pt-3 ">
               <div className="card-body p-4">
                 <img
                   src={vital}
                   className="rounded-circle img-fluid mx-auto d-block w-25"
                 />
-                <div className="data mt-3 ">
+                <div className="data-container mt-3 ">
                   <div className="d-flex justify-content-center" 
                   data-bs-toggle="tooltip" data-bs-placement="left" title="Doble click to edit!">
-                    <h3 onDoubleClick={() => ActivateEdit(1)} >{name}</h3>
+                    <h3 onDoubleClick={() => ActivateEdit(1) }className="data" >{name}</h3>
                   </div>
                   <div className="d-flex justify-content-center mt-1" 
                   data-bs-toggle="tooltip" data-bs-placement="left" title="Doble click to edit!">
-                    <h6 onDoubleClick={() => ActivateEdit(2)}>{description}</h6>
+                    <h6 onDoubleClick={() => ActivateEdit(2)} className="data">{description}</h6>
                   </div>
                   <div className="d-flex justify-content-center mt-3" 
                   data-bs-toggle="tooltip" data-bs-placement="left" title="Doble click to edit!">
-                    <p onDoubleClick={() => ActivateEdit(3)}>{about}</p>
+                    <p onDoubleClick={() => ActivateEdit(3)} className="data">{about}</p>
                   </div>
                 </div>
-                <div className=" d-flex " id="genre">
+                <div className=" d-flex d-flex justify-content-center" id="genre">
                   <p> Favorits Genres: </p>
-                  <p className="border w-100" onDoubleClick={() => ActivateEdit(4)} 
-                  data-bs-toggle="tooltip" data-bs-placement="left" title="Doble click to edit!">{favorites}</p>
+                  <p className="data border p-2 " onDoubleClick={() => ActivateEdit(4)} 
+                  data-bs-toggle="tooltip" title="Doble click to edit!">{favorites}</p>
                 </div>
               </div>
+              <div className="ps-3 pe-3 pb-1">{isActive ? <TextArea ActivateEdit={ActivateEdit} id={id} Edit={Edit} /> : ""}
+            </div>
             </article>
-          </div>
-
-          <div className="columna col-lg-4 ps-2 d-flex p-2 ">
-            <div className="mt-3 border rounded w-100">
+          </div>          
+          <div className="columna2 col-12 col-lg-6">
+            <div className="border rounded w-75">
               <h2>Favoritos</h2>
               <hr className="m-0" />
               <ul className="list-group" id="favs">
@@ -88,7 +88,7 @@ export const Profile = () => {
                       allowTransparency="true"
                       allow="encrypted-media"
                     ></iframe>
-                    <span><button className="btn " onClick={() => removeFromFavorites(index)}><i className="fa-solid fa-xmark"></i></button></span>
+                    <span><button className="btn rounded-circle" onClick={() => removeFromFavorites(index)}><i className="fa-solid fa-xmark "></i></button></span>
                   </li>
                 ))}
               </ul>
