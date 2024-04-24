d7f1c71f76396db27ea5ea8ad0191c28d11a5313
Virgi02i87167 <hvirgilio088@gmail.com>
2024-04-23 18:44:25 -0600

    cambios

--- src/components/pages/homepage/components/NovedadesList.jsx
+++ src/components/pages/homepage/components/NovedadesList.jsx
@@ -5,7 +5,7 @@ export const NovedadesList = ({ novedades }) => {
 
   const totalNovedades = novedades.length
 
-  const [novedadesPerPage, setNovedadesPerPage] = useState(2);
+  const [novedadesPerPage, setNovedadesPerPage] = useState(3);
   const [currentPage, setCurrentPage] = useState(1);
 
   const lastIndex = currentPage * novedadesPerPage
@@ -29,44 +29,27 @@ export const NovedadesList = ({ novedades }) => {
 
   return (
     <>
-      {/* <div className='container-novedades'>
-        {novedades.map((novedad) => (
-          <div className='card-novedades' key={novedad.id}>
-            <figure className='container-img'>
-              <img src={novedad.imagen} alt={novedad.title} />
-            </figure>
-            <div className='info-novedad'>
-              <h3>{novedad.title}</h3>
-              <p>{novedad.descripcion}</p>
-              <a href={novedad.ver} target="_blank"><button>Ver Mas</button></a>
-            </div>
-          </div>
-        )).slice(firstIndex, lastIndex)}
-      </div> */}
 
-<div className='container'>
-  <div className='row justify-content-center'>
-    <div className='col-md-8 offset-md-2'>
-      <div className='container-novedades'>
-        {novedades.map((novedad) => (
-          <div className='card-novedades' key={novedad.id}>
-            <figure className='container-img'>
-              <img src={novedad.imagen} alt={novedad.title} />
-            </figure>
-            <div className='info-novedad'>
-              <h3>{novedad.title}</h3>
-              <p>{novedad.descripcion}</p>
-              <a href={novedad.ver} target="_blank"><button>Ver Más</button></a>
+      <div className='container'>
+        <div className='row justify-content'>
+          <div className='col-md-8 offset-md-2'>
+            <div className='container-novedades'>
+              {novedades.map((novedad) => (
+                <div className='card-novedades' key={novedad.id}>
+                  <figure className='container-img'>
+                    <img src={novedad.imagen} alt={novedad.title} />
+                  </figure>
+                  <div className='info-novedad'>
+                    <h3>{novedad.title}</h3>
+                    <p>{novedad.descripcion}</p>
+                    <a href={novedad.ver} target="_blank"><button>Ver Más</button></a>
+                  </div>
+                </div>
+              )).slice(firstIndex, lastIndex)}
             </div>
           </div>
-        )).slice(firstIndex, lastIndex)}
+        </div>
       </div>
-    </div>
-  </div>
-</div>
-
-
-
       <Gaming 
         novedadesPerPage={novedadesPerPage}
         currentPage={currentPage}
